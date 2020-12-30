ac7e19a8f2c4d0c6cec36e32f9b4e37f4b3e8a40
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-30 02:50:45 +0200

    App general overview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Slack Clone</h1>
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React Router <<<>>> Chat Screen */}
     </div>
   );
 }
