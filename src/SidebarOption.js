9a25e57b874921e2c7aab97614a001957faa280f
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-31 04:37:29 +0200

    Reusing the same component for several use cases

---
+++ src/SidebarOption.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SidebarOption.css';
+
+function SidebarOption({ Icon, title }) {
+    return (
+        <div className="sidebarOption">
+            {/* Only render an Icon if an Icon exists */}
+            {Icon && <Icon className="sidebarOption__icon" />}
+            {/* 
+                If you pass in an Icon then render the title
+                But if you didn't pass in the title, l want it to be a channel
+            */}
+            {Icon ? <h3>{title}</h3> : <h3># {title}</h3>}
+        </div>
+    );
+}
+
+export default SidebarOption;
