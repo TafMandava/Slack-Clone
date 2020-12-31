65459d84e822ab55c0917025aee6a3af7ab957a6
Tafadzwa Mandava <tmandava@mezzanineware.com>
2020-12-31 05:22:08 +0200

    Adding more side options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,6 +11,8 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -33,6 +35,11 @@ function Sidebar() {
             <SidebarOption Icon={AppsIcon} title="Apps" />
             <SidebarOption Icon={FileCopyIcon} title="File browser" />
             <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Expand More" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" /> 
+            {/* Connect to DB and list all the channels */}         
         </div>
     );
 }
