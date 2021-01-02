fcb6c9d826248bec0c2b2c450ad3c1f1e43b18e1
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-02 21:56:13 +0200

    Initializing chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Chat.css';
+/*
+    Alternative - props.match.params
+    Hooks such as useState, useEffect can only be used in Functional Components
+*/
 import { useParams } from 'react-router-dom';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 function Chat() {
     /* 
@@ -12,6 +18,20 @@ function Chat() {
     return (
         <div className="chat">
             <h2>You are in the {roomId} room</h2>
+            {/* Chat header */}
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong>#general </strong>
+                        <StarBorderOutlinedIcon />
+                    </h4>
+                </div>
+                <div className="chat__headerRight">
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
