d6db3d6c7dc3516b84b22aa5008dde0f1144aeea
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-02 16:26:00 +0200

    Using Switch and Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Body */}
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* React Router <<<>>> Chat Screen */}
-      </div>
+      {/*
+          Router handles all the page routing
+      */}
+      <Router>
+        {/* Header */}
+        <Header />
+        {/* Body */}
+        <div className="app__body"> 
+          {/* Sidebar */}
+          <Sidebar />
+
+          {/* 
+              We always want to show the header 
+              Use a Switch to check the route that we are in
+              Based on the route that you are in, the switch that we are in
+              And based on the route that we are in we wil render the appropriate screen
+          */}
+          <Switch>
+            {/*
+                http://localhost:3000/room/abc
+            */}
+            <Route path="/room/:roomId">
+              <h1>Chat Screen</h1>
+            </Route>
+            {/*
+                http://localhost:3000/
+            */}
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>                     
+          </Switch>
+
+          {/* React Router <<<>>> Chat Screen */}
+        </div>
+        </Router>
     </div>
   );
 }
