ce16d48c564cc21f2ced5a069974a6c6663eafed
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-03 03:43:37 +0200

    Dynamically rendering the chat room names

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
 /*
     Alternative - props.match.params
@@ -7,13 +7,43 @@ import './Chat.css';
 import { useParams } from 'react-router-dom';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+/* 
+    https://usehooks.com/ 
+*/
+import db from './firebase';
 
 function Chat() {
+
+    const [roomDetails, setRoomDetails] = useState(null);
     /* 
         useParams hook
         used for Pulling the roomId parameter from the url 
     */
     const {roomId } = useParams();
+
+    useEffect(() => {
+        /* 
+            Only run this code if the roomId exists 
+        */
+        if (roomId) {
+            /* 
+                Use the roomId to get the room document
+                Go to the rooms collection and go to the doc and use the roomId inside the url to find the room that we are in
+            */
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
+                /*
+                    It should return for that document the data. That is the name
+                */
+                setRoomDetails(
+                    snapshot.data()
+                );
+            });
+
+        }
+
+    }, [roomId]); /* Update the code everytime the roomId changes */
+
+    console.log("Room Details >>> ", roomDetails);
 
     return (
         <div className="chat">
@@ -21,7 +51,7 @@ function Chat() {
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
-                        <strong># general </strong>
+                        <strong># {roomDetails.name}</strong>
                         <StarBorderOutlinedIcon />
                     </h4>
                 </div>
